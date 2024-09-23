0a2fa518e79f949bf86b94c7bbdb96eb44aad3e5
Man-Kaur <man.kaur86998@gmail.com>
2024-09-23 21:43:43 +0530

    Frontend for Authorisation and Authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,24 @@
-function App() {
- 
-  
-}
+import React from 'react'
+import './App.css'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Admins from './components/Admins'
+import Home from './components/Home'
 
-export default App
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/signup' element={<SignUp/>}></Route>
+      
+      <Route path='/admins' element={<Admins/>}></Route>
+      <Route path='/' element={<Home/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
