8591c9bd452d11ba69cb4f392778459a4fdeec37
Ritesh Raj Moond <moondritesh@gmail.com>
2024-09-23 19:15:54 +0530

    Added user routes

--- backend/main.js
+++ backend/main.js
@@ -4,6 +4,8 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const adminRoutes = require('./routes/admin-routes');
+const userRoutes = require('./routes/user-routes');
+
 const app = express();
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -12,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('api/v1/admin', adminRoutes);
+app.use('api/v1/user', userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);  
