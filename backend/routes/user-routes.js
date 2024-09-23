63f7ca9f43842a3b889515885b66bc69557956d3
Ritesh Raj Moond <moondritesh@gmail.com>
2024-09-23 19:21:03 +0530

    Added User Routes

---
+++ backend/routes/user-routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const userController = require('../controller/user-controller');
+
+const router = express.Router();
+
+router.post('/signup', userController.signup);
+router.post('/login', userController.login);
+
+router.get('/logout', userController.logout);
