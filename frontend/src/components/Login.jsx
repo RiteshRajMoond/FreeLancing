81885fe2956dcec93806e24c29c31d133b8861ea
Man-Kaur <man.kaur86998@gmail.com>
2024-09-23 21:45:36 +0530

    Frontend components added

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import { Box, Button, TextField, Typography } from "@mui/material"; 
+
+const Login = () => {
+  const vidStyle = { position: "fixed", width: "100%", height: "100%", top: "50%", left: "50%", objectFit: "cover", transform: "translate(-50%, -50%)", zIndex: "-1" };
+  const inputStyle = {margin: "10px 0", backgroundColor: "rgba(255, 255, 255, 0.7)", borderRadius: "10px",};
+  const buttonStyle = {backgroundColor: "#191f3d", color: "white",  padding: "10px",  marginTop: "20px",  "&:hover": { backgroundColor: "#d40000"}};
+  const boxStyle1 = {marginLeft:"470px", position: "relative", height: "100vh", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" };
+  const boxStyle2 = {position: "relative", zIndex: 1, backgroundColor: "rgba(255, 255, 255, 0.2)", padding: "40px", borderRadius: "10px", textAlign: "center", maxWidth: "400px", width: "100%", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)"};
+ 
+  const[name, setName] = useState("");
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  const navigate = useNavigate(); 
+
+  function handleSignup(e){
+    e.preventDefault();
+    navigate("/Home");
+  }
+
+  return (
+    <Box sx={boxStyle1}>
+      <video autoPlay loop muted style={vidStyle}> 
+        <source src='../../assets/login.mp4' type="video/mp4" /> 
+      </video>
+
+      <Box sx={boxStyle2}>
+        <Typography variant="h4" sx={{ color: "#fff", marginBottom: "20px" }}> Login </Typography>
+        <form onSubmit={handleSignup}>
+        <TextField onChange={(e)=>{setName(e.target.value)}} name='name' required label="Username" type='text' fullWidth style={inputStyle}/>
+        <TextField onChange={(e)=>{setPassword(e.target.value)}} name='password' required label="Password" type="password" fullWidth style={inputStyle} />
+        <Button type='submit' variant="contained" style={buttonStyle}>Login</Button>
+        </form>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
