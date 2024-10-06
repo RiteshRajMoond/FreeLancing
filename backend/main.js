9d773a426b5139756fa01c3ba9098ce4dc6c1aea
Man-Kaur <man.kaur86998@gmail.com>
2024-10-06 15:12:18 +0530

    Node  updation

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors'); 
 require('dotenv').config();
 
 const adminRoutes = require('./routes/admin-routes');
@@ -8,13 +9,14 @@ const userRoutes = require('./routes/user-routes');
 
 const app = express();
 
+app.use(cors());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.use('api/v1/admin', adminRoutes);
-app.use('api/v1/user', userRoutes);
+app.use('/api/v1/admin', adminRoutes);
+app.use('/api/v1/user', userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);  
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,53 +1,131 @@
-import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
-import axios from 'axios';
-import { Box, Button, TextField, Typography } from "@mui/material"; 
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const SignUp = () => {
-  const vidStyle = { position: "fixed", width: "100%", height: "100%", top: "50%", left: "50%", objectFit: "cover", transform: "translate(-50%, -50%)", zIndex: "-1" };
-  const inputStyle = {margin: "10px 0", backgroundColor: "rgba(255, 255, 255, 0.7)", borderRadius: "10px",};
-  const buttonStyle = {backgroundColor: "#191f3d", color: "white",  padding: "10px",  marginTop: "20px",  "&:hover": { backgroundColor: "#d40000"}};
-  const boxStyle1 = { position: "relative", height: "100vh", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center" };
-  const boxStyle2 = {position: "relative", zIndex: 1, backgroundColor: "rgba(255, 255, 255, 0.2)", padding: "40px", borderRadius: "10px", textAlign: "center", maxWidth: "400px", width: "100%", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)"};
- 
-  const[username, setUsername] = useState("");
-  const[email, setEmail] = useState("");
-  const[password, setPassword] = useState("");
-  // const [inviteJWT, setInviteJWT] = useState('');
-  const navigate = useNavigate(); 
+  const vidStyle = {
+    position: "fixed",
+    width: "100%",
+    height: "100%",
+    top: "50%",
+    left: "50%",
+    objectFit: "cover",
+    transform: "translate(-50%, -50%)",
+    zIndex: "-1",
+  };
+  const inputStyle = {
+    margin: "10px 0",
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    borderRadius: "10px",
+  };
+  const buttonStyle = {
+    backgroundColor: "#191f3d",
+    color: "white",
+    padding: "10px",
+    marginTop: "20px",
+    "&:hover": { backgroundColor: "#d40000" },
+  };
+  const boxStyle1 = {
+    position: "relative",
+    height: "100vh",
+    overflow: "hidden",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  const boxStyle2 = {
+    position: "relative",
+    zIndex: 1,
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    padding: "40px",
+    borderRadius: "10px",
+    textAlign: "center",
+    maxWidth: "400px",
+    width: "100%",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+  };
 
-  function handleSignup(e){
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [inviteJWT, setInviteJWT] = useState('');
+  const navigate = useNavigate();
+
+  function handleSignup(e) {
     e.preventDefault();
     // to communicate with backend API, http post request to send data from frontend to backend
-    axios.post("http://localhost:3001/signup", {username:username, email:email, password:password})
-    .then(res=>{
-          console.log("User created successfully");
-          navigate("/Login");
-    })
-    .catch(err=>{
-      if(err.response && err.response.status===400){
-        console.log("Bad Request: ", err.response.data.message);
-        alert("Bad Request: ", err.response.data.message);
-      }else{
+    axios
+      .post("http://localhost:3001/api/v1/user/signup", {
+        username: username,
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log("User created successfully");
+        navigate("/Login");
+      })
+      .catch((err) => {
+        // if (err.response && err.response.status === 400) {
+        //   console.log("Bad Request: ", err.response.data.message);
+        //   alert("Bad Request: ", err.response.data.message);
+        // } else {
+        //   console.log(err);
+        // }
         console.log(err);
-      }
-    });
+      });
   }
 
   return (
     <Box sx={boxStyle1}>
-      <video autoPlay loop muted style={vidStyle}> 
-        <source src='../../assets/signUp.mp4' type="video/mp4" /> 
+      <video autoPlay loop muted style={vidStyle}>
+        <source src="../../assets/signUp.mp4" type="video/mp4" />
       </video>
 
       <Box sx={boxStyle2}>
-        <Typography variant="h4" sx={{ color: "#fff", marginBottom: "20px" }}> Sign Up </Typography>
+        <Typography variant="h4" sx={{ color: "#fff", marginBottom: "20px" }}>
+          {" "}
+          Sign Up{" "}
+        </Typography>
         <form onSubmit={handleSignup}>
-        <TextField onChange={(e)=>{setUsername(e.target.value)}} name='name' required label="Username" type='text' fullWidth style={inputStyle}/>
-        <TextField onChange={(e)=>{setEmail(e.target.value)}} name='email' required label="Email" type="email"  fullWidth style={inputStyle} />
-        <TextField onChange={(e)=>{setPassword(e.target.value)}} name='password' required label="Password" type="password" fullWidth style={inputStyle} />
-        {/* <TextField onChange={(e) => setInviteJWT(e.target.value)} name="inviteJWT" label="Invite JWT" type="text" fullWidth style={inputStyle} /> */}
-        <Button type='submit' variant="contained" style={buttonStyle}> Sign Up</Button>
+          <TextField
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            name="name"
+            required
+            label="Username"
+            type="text"
+            fullWidth
+            style={inputStyle}
+          />
+          <TextField
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            name="email"
+            required
+            label="Email"
+            type="email"
+            fullWidth
+            style={inputStyle}
+          />
+          <TextField
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            name="password"
+            required
+            label="Password"
+            type="password"
+            fullWidth
+            style={inputStyle}
+          />
+          {/* <TextField onChange={(e) => setInviteJWT(e.target.value)} name="inviteJWT" label="Invite JWT" type="text" fullWidth style={inputStyle} /> */}
+          <Button type="submit" variant="contained" style={buttonStyle}>
+            {" "}
+            Sign Up
+          </Button>
         </form>
       </Box>
     </Box>
