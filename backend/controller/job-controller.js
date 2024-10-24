e4f1856b8c9d6dfd9098ff279b8b77c146169efc
Ritesh Raj Moond <moondritesh@gmail.com>
2024-10-24 20:50:44 +0530

    Added JobList Create-Job

---
+++ backend/controller/job-controller.js
@@ -0,0 +1,38 @@
+const Job = require("../models/Jobs");
+const User = require("../models/User");
+
+exports.createJob = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { title, description, requirements, budget, deadline } = req.body;
+
+    const newJob = new Job({
+      title,
+      description,
+      requirements,
+      budget,
+      deadline,
+      postedBy: userId,
+    });
+    await newJob.save();
+
+    const user = await User.findById(userId);
+    user.jobsPosted.push(newJob._id);
+    await user.save();
+
+    return res
+      .status(201)
+      .json({ message: "Job created successfully", job: newJob });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getJobs = async (req, res, next) => {
+  try {
+    const jobs = await Job.find().populate("postedBy", "name email");
+    return res.status(200).json({ jobs });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import './App.css'
-import Login from './components/Login'
-import SignUp from './components/SignUp'
-import Admins from './components/Admins'
-import Home from './components/Home'
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SuperAdmin from './components/SuperAdmin'
-import Dashboard from './components/Dashboard/Dashboard'
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Admins from "./components/Admins";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SuperAdmin from "./components/SuperAdmin";
+import Dashboard from "./components/Dashboard/Dashboard";
+import JobList from "./components/Jobs/JobList";
+import CreateJob from "./components/Jobs/CreateJob";
 
 export default function App() {
   return (
-    <>
     <BrowserRouter>
-    <Routes>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/signup' element={<SignUp/>}></Route>
-      <Route path='super-admin' element={<SuperAdmin/>}></Route>
-      <Route path='/admins' element={<Admins/>}></Route>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/dashboard' element={<Dashboard/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/super-admin" element={<SuperAdmin />} />
+        <Route path="/admins" element={<Admins />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/joblist" element={<JobList />} />
+        <Route path="/create-job" element={<CreateJob />} /> {/* Add route for AddJob */}
+      </Routes>
     </BrowserRouter>
-    </>
-  )
+  );
 }
