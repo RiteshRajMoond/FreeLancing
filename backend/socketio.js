e4e0277e051119fb8682d887cfecb5037bfec032
Ritesh Raj Moond <moondritesh@gmail.com>
2024-12-27 12:47:02 +0530

    Added Database for chats

--- backend/socketio.js
+++ backend/socketio.js
@@ -12,17 +12,17 @@ const setupSocket = (server) => {
 
   io.on("connection", (socket) => {
     socket.on("sendMsg", async (msg) => {
-      console.log(msg);
+      // console.log(msg);
       io.emit("receiveMsg", msg);
 
-      // const { jobId, senderName, text } = msg;
-      // const chat = await Chats.findOne({ jobId });
-      // if (chat) {
-      //   chat.messages.push({ senderName, text });
-      //   await chat.save();
-      // } else {
-      //   await Chats.create({ jobId, messages: [{ senderName, text }] });
-      // }
+      const { jobId, senderName = "Anonymous", text = "No message" } = msg;
+      const chat = await Chats.findOne({ jobId });
+      if (chat) {
+        chat.messages.push({ senderName, text });
+        await chat.save();
+      } else {
+        await Chats.create({ jobId, messages: [{ senderName, text }] });
+      }
     });
 
     socket.on("typing", (user) => {
