3c1cdb2cbe5f5ecd3c3023604d9c894488a86a1d
Man-Kaur <man.kaur86998@gmail.com>
2024-12-31 15:57:07 +0530

    trial

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -145,7 +145,7 @@ const Homepage = () => {
             position: "absolute",
             width: "100%",
             height: "100%",
-            backgroundImage: 'url("../../assets/moth.jpg")',
+            backgroundImage: `url(${require('../../assets/moth.jpg')})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundColor: "white",
