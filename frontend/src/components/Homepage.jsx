9b30a5523f637d79911aaf33f89e9d40419ffb7f
Man-Kaur <man.kaur86998@gmail.com>
2025-01-02 16:32:25 +0530

    trial

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -145,7 +145,7 @@ const Homepage = () => {
             position: "absolute",
             width: "100%",
             height: "100%",
-            backgroundImage: 'url("../../assets/moth.webp")',
+            backgroundImage: 'url("../../assets/moth.jpg")',
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundColor: "white",
